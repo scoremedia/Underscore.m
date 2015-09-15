f3d50a7ccb1624925e835063ea671d1dbc0892fa
A UiBrian <anluan@binarybuzzsoftware.com>
2015-09-15 03:29:18 -0400

    carthage works

---
+++ Underscore_m/Underscore_m.h
@@ -0,0 +1,19 @@
+//
+//  Underscore_m.h
+//  Underscore_m
+//
+//  Created by Anluan O'Brien on 9/15/15.
+//  Copyright © 2015 Robert Böhnke. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for Underscore_m.
+FOUNDATION_EXPORT double Underscore_mVersionNumber;
+
+//! Project version string for Underscore_m.
+FOUNDATION_EXPORT const unsigned char Underscore_mVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Underscore_m/PublicHeader.h>
+
+
