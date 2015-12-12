37f07e51bbfc25f59b1eb47b2dccfdc80b03cfe7
A UiBrian <anluan@binarybuzzsoftware.com>
2015-12-12 11:54:47 -0500

    adding ios target

---
+++ Underscore_m iOS/Underscore_m iOS.h
@@ -0,0 +1,19 @@
+//
+//  Underscore_m iOS.h
+//  Underscore_m iOS
+//
+//  Created by Anluan O'Brien on 12/10/15.
+//  Copyright © 2015 Robert Böhnke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Underscore_m iOS.
+FOUNDATION_EXPORT double Underscore_m_iOSVersionNumber;
+
+//! Project version string for Underscore_m iOS.
+FOUNDATION_EXPORT const unsigned char Underscore_m_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Underscore_m_iOS/PublicHeader.h>
+
+
